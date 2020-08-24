a66f8fc52cdf51ad4aa4bfbcc3eb7547262117a1
Kosarevic <kosarevic@gmail.com>
2020-08-24 10:31:43 +0200

    Implemented PanPizzaWindow (Main window), model and view model class.

---
+++ Zadatak_1/Model/PanPizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1.Model
+{
+    class PanPizza
+    {
+
+        public string Size { get; set; }
+        public List<string> Ingredients { get; set; }
+        public int Price { get; set; }
+
+    }
+}
---
+++ Zadatak_1/ViewModel/PanPizzaViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadatak_1.Model;
+
+namespace Zadatak_1.ViewModel
+{
+    class PanPizzaViewModel : INotifyPropertyChanged
+    {
+
+        public PanPizzaViewModel()
+        {
+
+        }
+
+        private PanPizza panPizza;
+
+        public PanPizza PanPizza
+        {
+            get { return panPizza; }
+            set
+            {
+                if (panPizza != value)
+                {
+                    panPizza = value;
+                    OnPropertyChanged("PanPizza");
+                }
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
